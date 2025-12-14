4b8faee603d920c24c2fd889fd8907921227a56a
joscript <yosefeid058@gmail.com>
2025-12-14 20:39:29 +0200

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       {/* <ToggleTheme /> */}
-      <Chart /> 
+      {/* <Chart />  */}
 
     </div>
   );
